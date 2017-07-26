f1d55466165653a9bb5ca5fa56f2f13decb6e461
PCM <congminh12000@gmail.com>
2017-07-26 20:52:41 +0700

    u

---
+++ application/modules/customer/views/tin-tuc.php
@@ -0,0 +1,62 @@
+<?php $this->load->view("partial/header"); ?>
+
+<div class="news">
+	<div class="container">
+    	<div class="row">
+        	<h4><a href="#" target="_self">Trang chủ</a>&nbsp;&nbsp;<span>&nbsp;&nbsp;<a href="#" target="_self">Tin tức tài chính</a>&nbsp;&nbsp;>&nbsp;&nbsp;<a href="#" target="_self">Tiền tệ</a></h4>
+        </div> <!--End row-->
+        <div class="row">
+        	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">
+            	<h1>TIỀN TỆ</h1>
+            </div>
+         </div> <!--End row-->
+        <div class="row">
+       	  <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 text-center box_news">
+            	<h2><a href="<?php echo $baseUrl . 'chi-tiet-tin-tuc'; ?>" target="_self">CONTENT CŨNG CHÍNH LÀ SẢN PHẨM - TÁC GIẢ: HỒ CÔNG HOÀI PHƯƠNG</a></h2>
+                <p>Viết bởi <b>Nguyễn Thọ</b> vào 25/07/2017 12:18</p>
+                <img src="<?php echo $baseUrl . 'static/customer/img/news/avatar-news.jpg'; ?>" alt="(Tiêu đề bài viết)" class="img-responsive">
+            	<p>Bài viết nằm trong cuốn Gam7 No.5 Content: Nội dung của tác giả Hồ Công Hoài Phương,...</p>
+                <h5><a href="#" target="_self">Xem thêm <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a></h5>
+          </div> <!--End col-->
+
+          <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 text-center box_news">
+            	<h2><a href="#" target="_self">CONTENT CŨNG CHÍNH LÀ SẢN PHẨM - TÁC GIẢ: HỒ CÔNG HOÀI PHƯƠNG</a></h2>
+                <p>Viết bởi <b>Nguyễn Thọ</b> vào 25/07/2017 12:18</p>
+                <img src="<?php echo $baseUrl . 'static/customer/img/news/avatar-news.jpg'; ?>" alt="(Tiêu đề bài viết)" class="img-responsive">
+            	<p>Bài viết nằm trong cuốn Gam7 No.5 Content: Nội dung của tác giả Hồ Công Hoài Phương,...</p>
+                <h5><a href="#" target="_self">Xem thêm <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a></h5>
+          </div> <!--End col-->
+
+          <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 text-center box_news">
+            	<h2><a href="#" target="_self">CONTENT CŨNG CHÍNH LÀ SẢN PHẨM - TÁC GIẢ: HỒ CÔNG HOÀI PHƯƠNG</a></h2>
+                <p>Viết bởi <b>Nguyễn Thọ</b> vào 25/07/2017 12:18</p>
+                <img src="<?php echo $baseUrl . 'static/customer/img/news/avatar-news.jpg'; ?>" alt="(Tiêu đề bài viết)" class="img-responsive">
+            	<p>Bài viết nằm trong cuốn Gam7 No.5 Content: Nội dung của tác giả Hồ Công Hoài Phương,...</p>
+                <h5><a href="#" target="_self">Xem thêm <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a></h5>
+          </div> <!--End col-->
+
+          <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 text-center box_news">
+            	<h2><a href="#" target="_self">CONTENT CŨNG CHÍNH LÀ SẢN PHẨM - TÁC GIẢ: HỒ CÔNG HOÀI PHƯƠNG</a></h2>
+                <p>Viết bởi <b>Nguyễn Thọ</b> vào 25/07/2017 12:18</p>
+                <img src="<?php echo $baseUrl . 'static/customer/img/news/avatar-news.jpg'; ?>" alt="(Tiêu đề bài viết)" class="img-responsive">
+            	<p>Bài viết nằm trong cuốn Gam7 No.5 Content: Nội dung của tác giả Hồ Công Hoài Phương,...</p>
+                <h5><a href="#" target="_self">Xem thêm <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a></h5>
+          </div> <!--End col-->
+
+        </div> <!--End row-->
+        <div class="row">
+        	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">
+                <ul class="pagination pagination-sm">
+                  <li><a href="#">1</a></li>
+                  <li><a href="#">2</a></li>
+                  <li><a href="#">3</a></li>
+                  <li><a href="#">4</a></li>
+                  <li><a href="#">5</a></li>
+                </ul>
+            </div> <!--End col-->
+        </div> <!--End row-->
+    </div> <!--End container-->
+</div> <!--End news-->
+
+<?php $this->load->view("partial/footer"); ?>
+
